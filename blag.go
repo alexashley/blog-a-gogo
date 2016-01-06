d97a5da38353a8238cb8dd055f7096f894596d4b
gleisner <ahashle2@illinois.edu>
2016-01-06 02:00:50 -0800

    meh

--- blag.go
+++ blag.go
@@ -6,50 +6,18 @@ import (
 	"io/ioutil"
 )
 
-var (
-	// default values for Resources struct
-	STATIC_DIR string = "/static/"
-	TMPL_DIR   string = "/tmpl/"
-	BASE_TMPL  string = "/tmpl/base.html"
-	ERROR_DIR  string = "/tmpl/error"
-	POST_DIR   string = "/post/"
-	POST_FN    string = "posts.json" // post metadata in json format
-	ROUTES_FN  string = "routes.json"
-	PORT_NUM   string = ":80" // default http port
-)
-
-type Resources struct {
-	StaticDir string // static files (e.g., css, js)
-	TmplDir   string // templates
-	BaseTmpl  string // site-wide template
-	ErrorDir  string // custom error responses. default: /tmpl/error/
-	PostDir   string // actual post files (extensions of the base template)
-}
-
 type Blog struct {
-	Paths  Resources
 	Routes map[string]string // maps URLs to files, e.g., site.com/about -> tmpl/site.html
 	Posts  []Post            // post objects in memory TODO: sort by date
-	Port   string
+	URL    string
 }
 
-func NewBlog(filename string) *Blog {
-	var paths Resources
-	if filename != "" {
-		LoadFromJSON(filename, &paths)
-	} else {
-		paths = Resources{StaticDir: STATIC_DIR,
-			TmplDir:  TMPL_DIR,
-			BaseTmpl: BASE_TMPL,
-			ErrorDir: ERROR_DIR,
-			PostDir:  POST_DIR}
-	}
+func NewBlog(routesFile string, postFile string, url string) *Blog {
 	routes := make(map[string]string)
 	posts := make([]Post, 0)
-	LoadFromJSON(ROUTES_FN, &routes)
-	LoadFromJSON(POST_FN, &posts)
-
-	return &Blog{Paths: paths, Routes: routes, Posts: posts, Port: PORT_NUM}
+	LoadFromJSON(routesFile, &routes)
+	LoadFromJSON(postFile, &posts)
+	return &Blog{Routes: routes, Posts: posts, URL: url}
 }
 
 func LoadFromJSON(filename string, obj interface{}) {
--- main.go
+++ main.go
@@ -1,26 +1,60 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"io"
 	"net/http"
+	"time"
 )
 
-var b *Blog
+var (
+	blog      *Blog
+	staticDir string
+)
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
-
+	filename := r.URL.Path[len(staticDir):]
+	if filename == "" {
+		http.NotFound(w, r)
+		return
+	}
+	f, err := http.Dir(staticDir[1:]).Open(filename)
+	if err != nil {
+		fmt.Println(filename + " does not exist")
+	} else {
+		file := io.ReadSeeker(f)
+		http.ServeContent(w, r, filename, time.Now(), file)
+	}
 }
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
-
+	// check for empty string
+	filename := blog.Routes[r.URL.Path[1:]]
+	if filename == "" {
+		fmt.Println("Unmapped resource " + r.URL.Path)
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Println(filename)
+	t, err := template.ParseFiles(filename)
+	if err != nil {
+		panic(err)
+	}
+	if err = t.Execute(w, blog); err != nil {
+		fmt.Println(err)
+	}
 }
 
 // TODO: read options from stdin
 // switches:
 // 	-p filename w/ all post metadata in JSON format
-// 	-c filename containing non-vanilla configuration
+//  -s path to static directory
+//  -r route file
 func main() {
-	b = NewBlog("")
-	http.HandleFunc(b.Paths.StaticDir, staticHandler)
+	blog = NewBlog("routes.json", "posts.json", "localhost/")
+	staticDir = "/static/"
+	http.HandleFunc(staticDir, staticHandler)
 	http.HandleFunc("/", pageHandler)
-	http.ListenAndServe(b.Port, nil)
+	http.ListenAndServe(":80", nil)
 }
