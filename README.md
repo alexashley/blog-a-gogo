8f197384b3a75a71e5b5f36326c9aca4ba36fb21
gleisner <ahashle2@illinois.edu>
2016-01-08 08:35:39 -0800

    can now generate posts and static pages

--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 
 Yet another static site generator in Go.
 
+## Dependencies
+
+- [YAML](https://github.com/go-yaml/yaml) 
+- [blackfriday](https://github.com/russross/blackfriday)
+
 ## Usage
 
-A small sample site is included. After building, run `./blog-a-gogo` to see the generated site 
+A small sample site is included. After building, run `./blog-a-gogo` to see the generated site running on port 8080 
+
+Blog posts should use the extension .post and Markdown syntax.  
+
+To add a HTML page, create a file ending in .tmpl and place it in the content directory.
 
 ## Configuration
 
--- blog-a-go.go
+++ blog-a-go.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"github.com/russross/blackfriday"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
+	//"net/http"
+	"gopkg.in/yaml.v2"
 	"os"
 	"path/filepath"
-	//"net/http"
 	"strings"
 	"time"
 )
@@ -27,11 +31,16 @@ var (
 )
 
 type Page struct {
-	Title  string
+	Info   PageInfo
 	Body   []byte
 	URL    string
-	cssDir string
-	Date   string
+	CSSDir string
+}
+
+type PageInfo struct {
+	Title string
+	Blurb string
+	Date  string
 }
 
 type WatchedFile struct {
@@ -41,19 +50,83 @@ type WatchedFile struct {
 func renderTemplate(w io.Writer, filename string, p Page) {
 	t := template.Must(template.ParseFiles(filename, *tmplDir+*baseTmpl))
 	if err := t.ExecuteTemplate(w, "base", p); err != nil {
-		log.Fatal("Error parsing template " + filename)
+		log.Fatal(err)
 	}
 }
 
-func renderHTML(b []byte) {
-
+func splitFile(filename string) (frontMatter string, content string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	seenFM := true
+	line := ""
+	for scanner.Scan() {
+		line = scanner.Text() + "\n"
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+		if scanner.Text() == "---" {
+			seenFM = !seenFM
+			continue
+		}
+		if seenFM {
+			content += line
+		} else {
+			frontMatter += line
+		}
+	}
+	return frontMatter, content
 }
 
-//
+func getOutputFilename(filename string) string {
+	newPath := strings.TrimPrefix(filename, *contentDir)
+	newPath = strings.TrimSuffix(newPath, ".tmpl")
+	newPath = strings.TrimSuffix(newPath, ".post")
+	return *outDir + newPath + ".html"
+}
+
 func processFile(filename string) {
-	var p Page
-	p.URL = *host
-	p.cssDir = *cssDir
+	frontMatter, content := splitFile(filename)
+	var info PageInfo
+	yaml.Unmarshal([]byte(frontMatter), &info)
+	var b []byte
+	p := Page{info, b, *host, *cssDir}
+	finalFilename := getOutputFilename(filename)
+	output, err := os.Create(finalFilename)
+	defer output.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if strings.Contains(filename, ".post") {
+		// parse markdown
+		p.Body = blackfriday.MarkdownCommon([]byte(content))
+		renderTemplate(output, *postTmpl, p)
+		//ioutil.WriteFile(finalFilename, html, 0755)
+
+	} else {
+		// render template
+		path := strings.Split(filename, "/")
+		tmpPrefix := path[len(path)-1]
+		tmpFile, err := ioutil.TempFile("", tmpPrefix)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer tmpFile.Close()
+		if err := ioutil.WriteFile(tmpFile.Name(), []byte(content), 0755); err != nil {
+			log.Fatal(err)
+		}
+		/*finalFilename := getOutputFilename(filename)
+		output, err := os.Create(finalFilename)
+		if err != nil {
+			log.Fatal(err)
+		}*/
+		renderTemplate(output, tmpFile.Name(), p)
+		//output.Close()
+		os.Remove(tmpFile.Name())
+	}
 }
 
 func isUnchanged(filename string, info os.FileInfo) bool {
@@ -63,12 +136,21 @@ func isUnchanged(filename string, info os.FileInfo) bool {
 
 func isUnsupported(path string, info os.FileInfo) bool {
 	isTemplate := strings.Contains(path, ".tmpl")
-	isMarkdown := strings.Contains(path, ".md")
-	return !(isTemplate || isMarkdown) || info.IsDir()
+	isPost := strings.Contains(path, ".post")
+	return !(isTemplate || isPost)
 }
 
 func walkFn(path string, info os.FileInfo, err error) error {
-	// ignore directories and unchanged files
+	if info.IsDir() {
+		if _, err := os.Stat(*outDir + path); os.IsNotExist(err) {
+			err = os.Mkdir(*outDir+info.Name(), 0755)
+			if err != nil {
+				log.Print(err)
+			}
+		}
+		return nil
+	}
+	// unchanged and unsupported files
 	if isUnsupported(path, info) || isUnchanged(path, info) {
 		return nil
 	}
@@ -82,10 +164,16 @@ func walkFn(path string, info os.FileInfo, err error) error {
 // *.md -> html -> rendered w/ postTmpl (inherits from baseTmpl)
 // .tmpl -> rendered w/ baseTmpl
 func generateSite() {
+	err := os.Mkdir(*outDir, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
 	filepath.Walk(*contentDir, walkFn)
 }
 
 func main() {
 	flag.Parse()
 	allFiles = make(map[string]WatchedFile)
+	//	os.RemoveAll(*outDir)
+	generateSite()
 }
