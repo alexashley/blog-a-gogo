37f8f1336df8078ec5fb08bc00ecf1432169c456
gleisner <ahashle2@illinois.edu>
2016-01-10 22:55:48 -0800

    separated server and site generator

--- blog-a-go.go
+++ blog-a-go.go
@@ -2,65 +2,146 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"github.com/russross/blackfriday"
 	"gopkg.in/yaml.v2"
 	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
 )
 
-var (
-	config    = flag.String("config", "config.yml", "Yaml configuration file")
-	clean     = flag.Bool("clean", false, "Remove outDir and dump file before running")
-	watch     = flag.Bool("watch", false, "Watch contentDir for changes")
-	runServer = flag.Bool("runServer", true, "Run server on -host, listening on -port")
-	host      = flag.String("host", "http://localhost:8080/", "Hostname.")
-	port      = flag.String("port", ":8080", "Port for http.ListenAndServe")
-
-	allFiles map[string]TrackedFile
-	settings Config
-)
-
 type Page struct {
-	Info    PageInfo
-	Body    template.HTML
-	URL     string // from the host flag
-	RelPath string // initialized for posts only
+	Info PageInfo
+	Body template.HTML
 }
 
 type PageInfo struct {
 	Title string
 	Blurb string
 	Date  string
+	Path  string
 }
 
-type TrackedFile struct {
-	Content  Page
-	LastSeen string
+type TFile struct {
+	Content  PageInfo
+	LastSeen string // last time the file was changed in time.RFC1123
 }
 
 type Config struct {
-	TmplDir    string // Directory containing Go html templates
-	TmplExt    string // Templates should end with this extension
-	BaseTmpl   string // Master template. Should be in TmplDir
-	PostTmpl   string // Template used for posts
-	IndexTmpl  string // Template for /
-	BlogTmpl   string // Template used for list of posts
-	PostExt    string // Posts should end with this extension
-	ContentDir string // Posts and static pages go here
-	OutDir     string // Processed files are placed in this directory
-	WatchFile  string // Information from previous runs is stored here
+	TmplDir     string // Directory containing Go html templates
+	TmplExt     string // Templates should end with this extension
+	BaseTmpl    string // Master template. Should be in TmplDir
+	PostTmpl    string // Template used for posts
+	IndexTmpl   string // Template for /
+	BlogTmpl    string // Template used for list of posts
+	PostExt     string // Posts should end with this extension
+	ContentDir  string // Posts and static pages go here
+	OutDir      string // Processed files are placed in this directory
+	DumpFile    string // Information from previous runs is stored here
+	ShouldWatch bool   // Watch the content directory for changes
 }
 
-func renderTemplate(w io.Writer, filename string, v interface{}) {
-	base := settings.TmplDir + settings.BaseTmpl
+type Generator struct {
+	Settings     Config // see config.yml
+	SeenChange   bool
+	TrackedFiles map[string]TFile // filename -> file metadata
+}
+
+// NewGenerator loads settings from the specified configuration file and may
+// remove all  previously generated files. Returns an error if the config file
+// couldn't be opened or if the file couldn't be marshaled (most likely due to a
+// YAML syntax error).
+func NewGenerator(configName string, shouldClean bool) (Generator, error) {
+	config, err := ioutil.ReadFile(configName)
+	var g Generator
+	if err != nil {
+		return g, err
+	}
+	//var c Config
+	if err = yaml.Unmarshal(config, &g.Settings); err != nil {
+		return g, err
+	}
+	g.TrackedFiles = make(map[string]TFile)
+	g.SeenChange = true
+	if shouldClean {
+		os.RemoveAll(g.Settings.OutDir)
+		os.Remove(g.Settings.DumpFile)
+	} else {
+		// load dump file from previous run
+		dump, ferr := ioutil.ReadFile(g.Settings.DumpFile)
+		if ferr == nil {
+			yaml.Unmarshal(dump, &g.TrackedFiles)
+		} else {
+			// if unable to load the dump file, re-render all the files
+			os.RemoveAll(g.Settings.OutDir)
+		}
+	}
+	return g, err
+}
+
+// FirstRun calls generate for the first time and sets a ticker to call generate
+// every 5 seconds (unless the user chose not to watch the content directory).
+func (g *Generator) FirstRun() {
+	g.generate()
+	if g.Settings.ShouldWatch {
+		ticker := time.NewTicker(time.Second * 5)
+		// BUG: if large amounts of new posts are processed each run
+		// there's a chance a call to generate might not finish before the
+		// ticker calls again. Fix: Put a mutex on each output file?
+		go func() {
+			for range ticker.C {
+				g.generate()
+			}
+		}()
+	}
+}
+
+type NoResourceError struct {
+	Request string
+}
+
+func (e NoResourceError) Error() string {
+	return "No mapping between " + e.Request + " and a generated file"
+}
+
+// FindResource finds the filename of the requested resource.
+// After blog a gogo's first run, the root directory might look like this
+// content/
+//		about.tmpl
+//		post/
+//			first.post
+//			good.post
+// out/
+// 		about.html
+//		post/
+//			first.html
+//			good.html
+// and a GET for http://site.com/about should return out/about.html.
+// The caller should strip any leading or trailing "/" and the hostname
+// (http.HandleFunc does this by default).
+func (g *Generator) FindResource(res string) (string, error) {
+	// if a file isn't tracked, but does exist (e.g., images), simply return it
+	if doesExist(res) {
+		return res, nil
+	}
+	if res == "" {
+		res = "index"
+	}
+	out := g.Settings.OutDir + res + ".html"
+	if !doesExist(out) {
+		return out, NoResourceError{Request: res}
+	}
+	return out, nil
+}
+
+// renderTemplate assumes the existence of a base template and that all other
+// templates implement whatever {{ define }} blocks are in the base template.
+func (g *Generator) renderTemplate(w io.Writer, filename string, v interface{}) {
+	base := g.Settings.TmplDir + g.Settings.BaseTmpl
 	t := template.Must(template.ParseFiles(filename, base))
 	if err := t.ExecuteTemplate(w, "base", v); err != nil {
 		log.Fatal(err)
@@ -69,6 +150,9 @@ func renderTemplate(w io.Writer, filename string, v interface{}) {
 	}
 }
 
+// splitFile breaks a given file up into front matter (a YAML block at the start
+// of the file) and the remaining file contents.
+// BUG: the actual terminator for a YAML block is "..."
 func splitFile(filename string) (frontMatter string, content string) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -96,36 +180,41 @@ func splitFile(filename string) (frontMatter string, content string) {
 	return frontMatter, content
 }
 
-func getOutputFilename(filename string) string {
-	newPath := strings.TrimPrefix(filename, settings.ContentDir)
-	newPath = strings.TrimSuffix(newPath, settings.TmplExt)
-	newPath = strings.TrimSuffix(newPath, settings.PostExt)
-	return settings.OutDir + newPath + ".html"
+// getOutputFilename returns the output filename for the given parameter
+func (g *Generator) getOutputFilename(filename string) string {
+	newPath := strings.TrimPrefix(filename, g.Settings.ContentDir)
+	newPath = strings.TrimSuffix(newPath, g.Settings.TmplExt)
+	newPath = strings.TrimSuffix(newPath, g.Settings.PostExt)
+	return g.Settings.OutDir + newPath + ".html"
 }
 
+// stripDirectories returns everything after the last / in the parameter
 func stripDirectories(filename string) string {
 	path := strings.Split(filename, "/")
 	return path[len(path)-1]
 }
 
-func processFile(filename string) Page {
+// processFile renders the Markdown for a post and calls renderTemplate() with
+// the post template OR simply calls renderTemplate() if the filename ends with
+// the template extension.
+func (g *Generator) processFile(filename string) Page {
 	frontMatter, content := splitFile(filename)
 	var info PageInfo
 	yaml.Unmarshal([]byte(frontMatter), &info)
-	p := Page{info, "", *host, ""}
-	finalFilename := getOutputFilename(filename)
+	p := Page{info, ""}
+	finalFilename := g.getOutputFilename(filename)
 	output, err := os.Create(finalFilename)
 	defer output.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if strings.Contains(filename, settings.PostExt) {
+	if strings.Contains(filename, g.Settings.PostExt) {
 		// parse markdown, render post template
 		p.Body = template.HTML(blackfriday.MarkdownCommon([]byte(content)))
-		p.RelPath = getOutputFilename(filename)
-		p.RelPath = strings.Replace(p.RelPath, ".html", "", 1)
-		p.RelPath = strings.Replace(p.RelPath, settings.OutDir, "", 1)
-		renderTemplate(output, settings.TmplDir+settings.PostTmpl, p)
+		p.Info.Path = g.getOutputFilename(filename)
+		p.Info.Path = strings.Replace(p.Info.Path, ".html", "", 1)
+		p.Info.Path = strings.Replace(p.Info.Path, g.Settings.OutDir, "", 1)
+		g.renderTemplate(output, g.Settings.TmplDir+g.Settings.PostTmpl, p)
 
 	} else {
 		// render template
@@ -138,96 +227,92 @@ func processFile(filename string) Page {
 		if err := ioutil.WriteFile(tmpFile.Name(), []byte(content), 0755); err != nil {
 			log.Fatal(err)
 		}
-		renderTemplate(output, tmpFile.Name(), p)
+		g.renderTemplate(output, tmpFile.Name(), p)
 		os.Remove(tmpFile.Name())
 	}
 	return p
 }
 
-func isUnchanged(filename string, info os.FileInfo) bool {
-	trackedFile, ok := allFiles[filename]
+// isUnchanged returns true if the modification time of the file matches the
+// time it was first recorded
+func (g *Generator) isUnchanged(filename string, info os.FileInfo) bool {
+	trackedFile, ok := g.TrackedFiles[filename]
 	modTime := info.ModTime().Format(time.RFC1123)
 	return ok && (trackedFile.LastSeen == modTime)
 }
 
-func isUnsupported(path string, info os.FileInfo) bool {
-	isTemplate := strings.Contains(path, settings.TmplExt)
-	isPost := strings.Contains(path, settings.PostExt)
+// isUnsupported checks if the filename contains a post or template extension
+func (g *Generator) isUnsupported(path string, info os.FileInfo) bool {
+	isTemplate := strings.Contains(path, g.Settings.TmplExt)
+	isPost := strings.Contains(path, g.Settings.PostExt)
 	return !(isTemplate || isPost)
 }
 
-func walkFn(path string, info os.FileInfo, err error) error {
-	if info.IsDir() {
-		if _, err := os.Stat(settings.OutDir + info.Name()); os.IsNotExist(err) {
-			err = os.Mkdir(settings.OutDir+info.Name(), 0755)
-			if err != nil {
-				log.Print(err)
+// makeVisit returns a function that filepath.Walk can call.
+func (g *Generator) makeVisit() func(string, os.FileInfo, error) error {
+	// this anonymous function is called by filepath.Walk and does 1 of 3 things
+	// - if there is a directory in contentDir, recreate it in outDir
+	// - if a file is unchanged or unsupported, ignore it
+	// - otherwise, render it
+	// TODO: fire off each call to processFile in its own goroutine
+	return func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			if _, err := os.Stat(g.Settings.OutDir + info.Name()); os.IsNotExist(err) {
+				err = os.Mkdir(g.Settings.OutDir+info.Name(), 0755)
+				if err != nil {
+					log.Print(err)
+				}
 			}
+			return nil
 		}
+		if g.isUnsupported(path, info) || g.isUnchanged(path, info) {
+			return nil
+		}
+		p := g.processFile(path)
+		g.TrackedFiles[path] = TFile{p.Info, info.ModTime().Format(time.RFC1123)}
+		g.SeenChange = true
 		return nil
 	}
-	if isUnsupported(path, info) || isUnchanged(path, info) {
-		return nil
-	}
-	p := processFile(path)
-	allFiles[path] = TrackedFile{p, info.ModTime().Format(time.RFC1123)}
-
-	return nil
 }
 
-func generateSite() {
-	if _, err := os.Stat(settings.OutDir); os.IsNotExist(err) {
-		err := os.Mkdir(settings.OutDir, 0755)
+// generate simply begins the walk through the filesystem and dumps the results
+// into a file if there have been any changes
+func (g *Generator) generate() {
+	if _, err := os.Stat(g.Settings.OutDir); os.IsNotExist(err) {
+		err := os.Mkdir(g.Settings.OutDir, 0755)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	filepath.Walk(settings.ContentDir, walkFn)
-	info, _ := yaml.Marshal(&allFiles)
-	ioutil.WriteFile(settings.WatchFile, info, 0664)
-	generateBlog()
+	filepath.Walk(g.Settings.ContentDir, g.makeVisit())
+	if g.SeenChange {
+		g.makeBlog()
+		g.SeenChange = false
+		info, _ := yaml.Marshal(&g.TrackedFiles)
+		ioutil.WriteFile(g.Settings.DumpFile, info, 0664)
+	}
 }
 
+// doesExist is a wrapper around os.Stat(filename). Technically, the return
+// value should be os.IsExist(err), but blog a gogo can't handle any filesystem
+// errors that os.Open() or os.Create() might return, so checking for nil is
+// the best that can be done
 func doesExist(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	filename := strings.Trim(r.URL.Path, "/")
-	outf := settings.OutDir + filename + ".html"
-	// if the file is tracked, load it from the out directory
-	if filename == "" {
-		filename = settings.OutDir + "index.html"
-	}
-	if doesExist(outf) {
-		filename = outf
-	}
-	log.Print(filename + " requested")
-	f, err := http.Dir("").Open(filename)
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-	file := io.ReadSeeker(f)
-	http.ServeContent(w, r, filename, time.Now(), file)
-}
-
-type Blog struct {
-	Pages []Page
-	URL   string
-}
-
-// get a list o' posts and organize them
-func generateBlog() {
-	pages := make([]Page, 0)
-	for filename, p := range allFiles {
-		if strings.Contains(filename, settings.PostExt) {
+// makeBlog calls renderTemplate with a list of posts and the blog template
+// TODO: sort posts by date
+func (g *Generator) makeBlog() {
+	pages := make([]PageInfo, 0)
+	for filename, p := range g.TrackedFiles {
+		if strings.Contains(filename, g.Settings.PostExt) {
 			pages = append(pages, p.Content)
 		}
 	}
-	finalFilename := getOutputFilename(settings.BlogTmpl)
-	if _, err := os.Stat(finalFilename); err == nil {
+	finalFilename := g.getOutputFilename(g.Settings.BlogTmpl)
+	if doesExist(finalFilename) {
 		os.Remove(finalFilename)
 	}
 	f, e := os.Create(finalFilename)
@@ -235,47 +320,5 @@ func generateBlog() {
 		log.Fatal(e)
 	}
 	defer f.Close()
-	b := Blog{pages, *host}
-	renderTemplate(f, settings.TmplDir+settings.BlogTmpl, b)
-}
-
-func loadConfig(filename string) {
-	config, err := ioutil.ReadFile(filename)
-	if err != nil {
-		log.Fatal("Unable to load configuration file")
-	}
-	yaml.Unmarshal(config, &settings)
-}
-
-func main() {
-	flag.Parse()
-	loadConfig(*config)
-	if *clean {
-		os.RemoveAll(settings.OutDir)
-		os.Remove(settings.WatchFile)
-	}
-	allFiles = make(map[string]TrackedFile)
-	// blog-a-gogo has been run before. Load previous state of allFiles
-	if _, err := os.Stat(settings.WatchFile); err == nil {
-		info, err := ioutil.ReadFile(settings.WatchFile)
-		// ignore any errors, just rebuild allFiles and templates
-		if err == nil {
-			yaml.Unmarshal(info, &allFiles)
-		} else {
-			os.RemoveAll(settings.OutDir)
-		}
-	}
-	generateSite()
-	if *watch {
-		ticker := time.NewTicker(time.Second * 5)
-		go func() {
-			for _ = range ticker.C {
-				generateSite()
-			}
-		}()
-	}
-	if *runServer {
-		http.HandleFunc("/", handler)
-		http.ListenAndServe(*port, nil)
-	}
+	g.renderTemplate(f, g.Settings.TmplDir+g.Settings.BlogTmpl, pages)
 }
---
+++ server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var (
+	config    = flag.String("config", "config.yml", "YAML configuration file")
+	clean     = flag.Bool("clean", false, "Remove all files from previous runs.")
+	port      = flag.String("port", ":8080", "port number")
+	runServer = flag.Bool("runServer", false, "Run server on -port")
+	site      Generator
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	req := strings.Trim(r.URL.Path, "/")
+	fname, resErr := site.FindResource(req)
+	if resErr != nil {
+		log.Print("Unknown resource " + req + " requested")
+		http.NotFound(w, r)
+		return
+	}
+	log.Print("file " + req + " requested and " + fname + " found")
+	f, err := http.Dir("").Open(fname)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	file := io.ReadSeeker(f)
+	http.ServeContent(w, r, fname, time.Now(), file)
+}
+
+func init() {
+	flag.Parse()
+	var err interface{}
+	site, err = NewGenerator(*config, *clean)
+	if err != nil {
+		log.Fatal(err)
+	}
+	site.FirstRun()
+}
+
+func main() {
+	if *runServer {
+		http.HandleFunc("/", handler)
+		http.ListenAndServe(*port, nil)
+	}
+}
